bb032da8af4f8d397cff00c4e0f95ac9d405f94b
Luca Ferroni <luca@befair.it>
2018-05-09 12:23:30 +0200

    Lasciato commento solo sulle variabili "short"

--- demo-overflow.c
+++ demo-overflow.c
@@ -3,7 +3,7 @@
 
 
 int main() {
-    int values[3] = {1, 10, 50000};
+    int values[] = {1, 255, 256, 32767, 32768, 65535, 65536};
 
     char c;
     unsigned char uc;
@@ -12,8 +12,8 @@ int main() {
     short si;
     unsigned short usi;
 
-    int idx=0;
-    for (idx=0; idx<3; idx++) {
+    int idx;
+    for (idx=0; idx<7; idx++) {
         c = values[idx];
         uc = values[idx];
         i = values[idx];
@@ -21,11 +21,22 @@ int main() {
         si = values[idx];
         usi = values[idx];
         
+        // Visualizza a video il valore
 		printf("\n*** VALORE %i *** \n", values[idx]);
-        printf("char (%i) = %i\n", sizeof(c), c);
-        printf("unsigned char (%i) = %i\n", sizeof(uc), uc);
-        printf("int (%i) = %i\n", sizeof(i), i);
-        printf("unsigned int (%i) = %i\n", sizeof(ui), ui);
+
+        // Visualizza a video:
+        // - il tipo di variabile
+        // - la dimensione di ogni variabile (byte dedicati al corrispondente tipo di dato)
+        // - il valore interpretato
+
+        // I tipi di dati char e int vengono commentati per facilitare l'esercitazione
+        // Ogni alunno può disabilitare ulteriori tipi di dato man mano che ci vuole ragionare sopra
+
+        // printf("char (%i) = %i\n", sizeof(c), c);
+        // printf("unsigned char (%i) = %i\n", sizeof(uc), uc);
+        // printf("int (%i) = %i\n", sizeof(i), i);
+        // printf("unsigned int (%i) = %i\n", sizeof(ui), ui);
+
         printf("short (%i) = %i\n", sizeof(si), si);
         printf("unsigned short (%i) = %i\n", sizeof(usi), usi);
     }
